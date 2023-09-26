b19c4c5731c4f29ff5d66acbe6b71d9eda260702
GYALLERHORN <kjins1998@gmail.com>
2023-09-26 22:04:27 +0900

    생성된 맵을 저장할 SO 작성 중
    랜덤으로 생성된 맵을 하나의 고정된 던전으로 만들기 위한 작업 중입니다.

---
+++ A07_Roguelike/Assets/Editor_Maps/RandomDungeonGeneratorEditor.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor; // UnityEditor 네임스페이스
+
+[CustomEditor(typeof(AbstractDungeonGenerator), true)]
+public class RandomDungeonGeneratorEditor : Editor
+{
+    AbstractDungeonGenerator generator;
+
+    private void Awake()
+    {
+        generator = (AbstractDungeonGenerator)target; // generator는 AbstractDungeonGenerator를 가리킨다.
+        // generator = new SimpleWalkDungeonGenerator(); // 왜 이렇게는 안 쓴거지?
+        // generator = new AbstractDungeonGenerator(); //추상 형식 클래스/인터페이스는 인스턴스할 수 없다.
+    }
+
+    public override void OnInspectorGUI() // Graphical User Interface : 기능을 그래픽(아이콘 등)으로 나타낸 것
+    {                                      // InspectorGUI : Inspector창에 있는 GUI
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Create Dungeon"))
+        {
+            generator.GenerateDungeon();
+        }
+    }
+}
