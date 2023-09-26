1462cc20e2af267d259063ad3490179498ca2f72
Hwan007 <ghksdlco@gmail.com>
2023-09-26 11:58:28 +0900

    UI 작업 시작

--- A07_Roguelike/Assets/Scripts/Managers/UIManager.cs
+++ A07_Roguelike/Assets/Scripts/Managers/UIManager.cs
@@ -1,8 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem.XInput;
 
 public class UIManager : MonoBehaviour
 {
-    
+    [HideInInspector] public static UIManager Instance;
+
+    private List<UIBase> UIOpened = new List<UIBase>();
+
+    private ObjectPool _pools;
 }
