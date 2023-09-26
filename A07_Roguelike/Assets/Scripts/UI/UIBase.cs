347aa5e188b5295761f58d741faf6a51b923bf13
Hwan007 <ghksdlco@gmail.com>
2023-09-26 16:57:28 +0900

    UI Popup 완성.

--- A07_Roguelike/Assets/Scripts/UI/UIBase.cs
+++ A07_Roguelike/Assets/Scripts/UI/UIBase.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 public class UIBase : MonoBehaviour
 {
     [SerializeField] protected RectTransform _rectTransform;
-    [SerializeField] protected Canvas _canvas;
-
-    protected bool _isTemp;
 
     public virtual void Refresh()
     {
