9bc258745c90659d9e153032f81c1ebf1da15e26
GYALLERHORN <kjins1998@gmail.com>
2023-09-29 20:01:54 +0900

    복도의 가장자리deadEnd에 반드시 방 생성 기능 완료

--- A07_Roguelike/Assets/Jin_Works/Scripts_Maps/CorridorFirstDungeonGenerator.cs
+++ A07_Roguelike/Assets/Jin_Works/Scripts_Maps/CorridorFirstDungeonGenerator.cs
@@ -29,10 +29,63 @@ public class CorridorFirstDungeonGenerator : SimpleWalkDungeonGenerator
 
         HashSet<Vector2Int> roomPositions = createRooms(potentialRoomrPositions);
 
+        List<Vector2Int> deadEnds = FindAllDeadEnds(floorPositions); // 복도의 가장자리 좌표, 가장자리에 방이 없으면 그 복도는 의미가 없다.
+
+        CreateRoomsAtDeadEnds(deadEnds, roomPositions); // 복도 가장자리에 방 만들기
+
         floorPositions.UnionWith(roomPositions); // 위 메서드로 생성된 방 좌표도 복도 좌표와 함께 바닥 타일을 생성할 좌표다.
 
         tilemapVisualizer.PaintFloorTile(floorPositions);
         WallGenerator.CreateWalls(floorPositions, tilemapVisualizer);
+    }
+
+    private void CreateRoomsAtDeadEnds(List<Vector2Int> deadEnds, HashSet<Vector2Int> roomFloors)
+    {
+        foreach (var position in deadEnds)
+        {
+            if (!roomFloors.Contains(position))
+            {
+                var room = RunRandomWalk(randomWalkParameters, position);
+                roomFloors.UnionWith(room);
+            }
+        }
+    }
+
+    private List<Vector2Int> FindAllDeadEnds(HashSet<Vector2Int> floorPositions) // 복도 좌표 집합을 대상으로 복도의 가장자리 좌표 찾기
+    {
+        List<Vector2Int> deadEnds = new List<Vector2Int>();
+        foreach (var position in floorPositions)
+        {
+            int neighboursCount = 0;
+            foreach (var direction in Direction2D.cardinalDirectionsList) // 복도 좌표 집합 요소 하나하나 상하좌우 다 반복해서
+            {
+                if (floorPositions.Contains(position + direction))
+                {
+                    neighboursCount++;
+                }
+            }
+
+            if (neighboursCount == 1) // 복도 요소 1개에 인접한 좌표가 1개 밖에 없으면
+            {
+                deadEnds.Add(position); // 그건 복도의 가장자리
+            }
+        }
+        return deadEnds;
+    }
+
+    private void CreateCorridors(HashSet<Vector2Int> floorPositions, HashSet<Vector2Int> potentialRoomrPositions)
+    {
+        var currentPosition = startPosition; // 0,0부터 시작
+        potentialRoomrPositions.Add(currentPosition); // 물론, 시작좌표에도 방이 만들어질 수 있다.
+
+        for (int i = 0; i < corridorCount; i++)
+        {
+            var corridor = ProceduralGenerationAlgorithms.RandomWalkCorridor(currentPosition, corriderLength); // corridor 좌표 집합 생성 명령
+            floorPositions.UnionWith(corridor); // 좌표집합 합치기
+
+            currentPosition = corridor[corridor.Count - 1]; // 다음 corridor 좌표 생성의 시작점은 현재까지 만들어진 corrider의 마지막 좌표
+            potentialRoomrPositions.Add(currentPosition);
+        }
     }
 
     private HashSet<Vector2Int> createRooms(HashSet<Vector2Int> potentialRoomPositions)
@@ -49,20 +102,5 @@ public class CorridorFirstDungeonGenerator : SimpleWalkDungeonGenerator
             roomPositions.UnionWith(roomFloor);
         }
         return roomPositions;
-    }
-
-    private void CreateCorridors(HashSet<Vector2Int> floorPositions, HashSet<Vector2Int> potentialRoomrPositions)
-    {
-        var currentPosition = startPosition; // 0,0부터 시작
-        potentialRoomrPositions.Add(currentPosition); // 물론, 시작좌표에도 방이 만들어질 수 있다.
-
-        for (int i = 0; i < corridorCount; i++)
-        {
-            var corridor = ProceduralGenerationAlgorithms.RandomWalkCorridor(currentPosition, corriderLength); // corridor 좌표 집합 생성 명령
-            floorPositions.UnionWith(corridor); // 좌표집합 합치기
-
-            currentPosition = corridor[corridor.Count - 1]; // 다음 corridor 좌표 생성의 시작점은 현재까지 만들어진 corrider의 마지막 좌표
-            potentialRoomrPositions.Add(currentPosition);
-        }
     }
 }
