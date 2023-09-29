700b017ff44097208d994f4786c9a14b49bc0cda
GYALLERHORN <kjins1998@gmail.com>
2023-09-29 22:37:42 +0900

    맵 초기화
    Scene뷰의 Test용 맵 지웠습니다.
    ClearTilemap UI도 만들어야겠는데

--- A07_Roguelike/Assets/Jin_Works/Editor_Maps/RandomDungeonGeneratorEditor.cs
+++ A07_Roguelike/Assets/Jin_Works/Editor_Maps/RandomDungeonGeneratorEditor.cs
@@ -12,7 +12,7 @@ public class RandomDungeonGeneratorEditor : Editor
     {
         generator = (AbstractDungeonGenerator)target; // generator는 AbstractDungeonGenerator를 가리킨다.
         // generator = new SimpleWalkDungeonGenerator(); // 왜 이렇게는 안 쓴거지?
-        // generator = new AbstractDungeonGenerator(); //추상 형식 클래스/인터페이스는 인스턴스할 수 없다.
+        // generator = new AbstractDungeonGenerator(); // 추상 형식 클래스/인터페이스는 인스턴스할 수 없다.
     }
 
     public override void OnInspectorGUI() // Graphical User Interface : 기능을 그래픽(아이콘 등)으로 나타낸 것
@@ -24,4 +24,5 @@ public class RandomDungeonGeneratorEditor : Editor
             generator.GenerateDungeon();
         }
     }
+
 }
