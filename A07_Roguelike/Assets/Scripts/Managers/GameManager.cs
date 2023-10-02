48e5ede6114d09353d239a6fcfcf592910ffb319
Hwan007 <ghksdlco@gmail.com>
2023-10-02 23:19:13 +0900

    버그 수정 및 마을 씬 생성

--- A07_Roguelike/Assets/Scripts/Managers/GameManager.cs
+++ A07_Roguelike/Assets/Scripts/Managers/GameManager.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [HideInInspector] public static GameManager Instance;
-    public Canvas UICanvas;
+    [HideInInspector] public Canvas UICanvas;
 
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
+            DontDestroyOnLoad(gameObject);
         }
     }
 
-    public void StartGame()
+    private void LateUpdate()
     {
-
+        if (UICanvas == null)
+        {
+            UICanvas = GameObject.Find("UI")?.GetComponent<Canvas>();
+        }
     }
 
     public void StartDunegeon()
