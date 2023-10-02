43e575ca25134da9969230def4455ee4617631b5
Hwan007 <ghksdlco@gmail.com>
2023-10-02 12:23:59 +0900

    프리펩을 필요한 경우만 로드하도록 수정. 메뉴 UI 완성.

---
+++ A07_Roguelike/Assets/Scripts/UI/UIMenu.cs
@@ -0,0 +1,48 @@
+using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIMenu : UIBase
+{
+    [SerializeField] private RectTransform _menuRoot;
+    [Header("애니메이션을 위한 요소")]
+    [SerializeField] private RectTransform _menuFrame;
+    private UIInfo _statusUI;
+
+    private void Start()
+    {
+        Initialize();
+    }
+
+    public void Initialize()
+    {
+        AddMenu("상태창", () =>
+        {
+            // var ui = UIManager.ShowUI(eUIType.Status) as UIInfo;
+            // _statusUI = ui;
+            // _statusUI.Initialize();
+        });
+        AddMenu("다시 시작", () =>
+        {
+            // LoadScene("MainScene");
+        });
+        AddMenu("설정", () =>
+        {
+
+        });
+
+        _menuFrame.DOSizeDelta(Vector2.zero, 0.4f).SetEase(Ease.OutBack);
+    }
+
+    public UIMenuItem AddMenu(string title, Action openAtClick)
+    {
+        var menu = UIManager.ShowUI<UIMenuItem>(_menuRoot);
+        menu.Initialize(title, openAtClick);
+        return menu;
+    }
+
+
+}
