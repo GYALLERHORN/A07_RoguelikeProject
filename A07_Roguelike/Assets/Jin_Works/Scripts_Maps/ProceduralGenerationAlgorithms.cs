77ef95dab9a1cf7780b45fdc1bd89b420d80fab1
GYALLERHORN <kjins1998@gmail.com>
2023-10-02 23:06:12 +0900

    Wall 작업 완료

--- A07_Roguelike/Assets/Jin_Works/Scripts_Maps/ProceduralGenerationAlgorithms.cs
+++ A07_Roguelike/Assets/Jin_Works/Scripts_Maps/ProceduralGenerationAlgorithms.cs
@@ -108,9 +108,27 @@ public static class Direction2D
     public static List<Vector2Int> cardinalDirectionsList = new List<Vector2Int>() // cardinal : 기본적인, 가장 중요한
         {
             new Vector2Int(0,1), // UP
-            new Vector2Int(0,-1), // DOWN
             new Vector2Int(1,0), // RIGHT
+            new Vector2Int(0,-1), // DOWN
             new Vector2Int(-1,0), // LEFT
+        }; 
+    public static List<Vector2Int> diagonalDirectionsList = new List<Vector2Int>() // diagonal : 사선의, 대각선의
+        {
+            new Vector2Int(1,1), // UP-RIGHT
+            new Vector2Int(1,-1), // RIGHT-DOWN
+            new Vector2Int(-1,-1), // DOWN-LEFT
+            new Vector2Int(-1,1), // LEFT-UP
+        };
+    public static List<Vector2Int> eightDirectionsList = new List<Vector2Int>() // 벽 좌표를 중심으로 인접한 8방향의 관계에 따라 벽 타일이 달라진다.
+        {
+            new Vector2Int(0,1), // UP
+            new Vector2Int(1,1), // UP-RIGHT
+            new Vector2Int(1,0), // RIGHT
+            new Vector2Int(1,-1), // RIGHT-DOWN
+            new Vector2Int(0,-1), // DOWN
+            new Vector2Int(-1,-1), // DOWN-LEFT
+            new Vector2Int(-1,0), // LEFT
+            new Vector2Int(-1,1), // LEFT-UP
         };
 
     public static Vector2Int GetRandomCardinalDirection()
