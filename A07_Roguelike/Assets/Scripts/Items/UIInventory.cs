bc81c0c5d5a6de3b9ee941e88c25838414982191
kksoo0131 <a67636521@gmail.com>
2023-10-04 17:19:41 +0900

    빌드 세팅

--- A07_Roguelike/Assets/Scripts/Items/UIInventory.cs
+++ A07_Roguelike/Assets/Scripts/Items/UIInventory.cs
@@ -1,8 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Progress;
-
 public class UIInventory : UIBase
 {
     public List<Item> itemList;
--- A07_Roguelike/Assets/Scripts/Managers/UIManager.cs
+++ A07_Roguelike/Assets/Scripts/Managers/UIManager.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEditor.Recorder.OutputPath;
 
 public enum eUIType
 {
