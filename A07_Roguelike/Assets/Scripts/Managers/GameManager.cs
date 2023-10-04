e70823445c2b547e468e9004c6e8b19db6d6a88b
Hwan007 <ghksdlco@gmail.com>
2023-10-04 15:17:53 +0900

    스탯 전송

--- A07_Roguelike/Assets/Scripts/Managers/GameManager.cs
+++ A07_Roguelike/Assets/Scripts/Managers/GameManager.cs
@@ -107,11 +107,12 @@ public class GameManager : MonoBehaviour
 
     private void TransferData()
     {
-        //PlayerInActive.GetComponent<CharacterStatsHandler>().;
+        PlayerInActive.GetComponent<CharacterStatsHandler>().statsModifiers = _characterStatsHandler.statsModifiers;
         PlayerInActive.GetComponent<HealthController>().LoadHealthController(_healthController);
         PlayerInActive.GetComponent<InventoryHandler>().Initialize(_Inventory);
 
         _characterStatsHandler = PlayerInActive.GetComponent<CharacterStatsHandler>();
+        _characterStatsHandler.UpdateCharacterStats();
         _Inventory = PlayerInActive.GetComponent<InventoryHandler>();
         _healthController = PlayerInActive.GetComponent<HealthController>();
     }
